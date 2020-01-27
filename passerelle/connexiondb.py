6f0cb73fa0415ec7ee3401d39c39a1532f19d6c5
Amandine01 <amandine.ducruet@free.fr>
2020-01-27 15:11:41 +0100

    infinite loop in connexiondb.py

--- passerelle/connexiondb.py
+++ passerelle/connexiondb.py
@@ -3,7 +3,7 @@ from urllib.parse import quote_plus
 
 if __name__ == "__main__":
     
-    print("coucou mon ami. on va tester mongodb hehe")
+    print("Connexion aux dbs")
     
     rpi_client = MongoClient("mongodb+srv://pfe_smartconstruction:pfe1961@smartconstruction-vkl5p.mongodb.net")
     rpi_client_local = MongoClient() #localhost
@@ -12,34 +12,18 @@ if __name__ == "__main__":
     db_local = rpi_client_local["messages"]
     collection = db["Capteurs"]
     collection_local = db_local["ReceivedData"]
-    
 
-    #try:
-    #    db.command("serverStatus")
-    #except Exception as e: print(e)
-    #else:
-    #    print("You are connected!")
-   
-    # Insert business object directly into MongoDB via insert
-    #dico = [
-    	#{"nom_capteur": 7, "CO" : 7, "NO2" : 7, "PM10" : 7, "PM25" : 7, "Son" : 7},
-	#{"nom_capteur": 2, "CO" : 2, "NO2" : 2, "PM10" : 2, "PM25" : 2, "Son" : 2}
-    #]
+    while (true):
+	    capteur_local = db_local.ReceivedData.find()
 
+	    #On rentre les données dans la db sur serveur
+	    resultat = collection.insert_many(capteur_local)
+	    print("Entered data : " + str(resultat))
 
-    #resultat_local = collection_local.insert_many(dico)
-    #print(resultat_local)
-        
-    #capteur = db.Capteurs.find_one({'nom_capteur': 18})
-    capteur_local = db_local.ReceivedData.find()
-    #print("essai d afficher une ligne de la base de donnees : " + str(capteur))
-
-    #On la rentre dans la db sur serveur
-    resultat = collection.insert_many(capteur_local)
-    print("Affichage de la valeur rentree : " + str(resultat))
-
-    effacer = collection_local.delete_many({})
-    print(effacer.deleted_count, "documents deleted")
+	    #On efface la db local
+	    effacer = collection_local.delete_many({})
+	    print(effacer.deleted_count, "Local data deleted")
 
     
     rpi_client.close()
+    rpi_client_local.close()
